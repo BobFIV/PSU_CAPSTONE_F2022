84bade3f684a4a2b566f93548c73e2baea12f077
NoahSchiro <noahschiro@rocketmail.com>
2022-10-08 17:02:20 -0400

    Added unit tests

--- tester/tester.py
+++ tester/tester.py
@@ -1,13 +1,10 @@
 import time
 import serial
 
-# configure the serial connections (the parameters differs on the device you are connecting to)
+# Configure the serial connections (the parameters differs on the device you are connecting to)
 ser = serial.Serial(
     port='/dev/ttyACM0',
-    baudrate=9600,                  #TODO
-    parity=serial.PARITY_ODD,       #TODO
-    stopbits=serial.STOPBITS_TWO,   #TODO
-    bytesize=serial.SEVENBITS       #TODO
+    baudrate=115200                #TODO: I believe this is correct
 )
 
 # Check if the serial port is open
@@ -16,27 +13,33 @@ if ser.is_open:
 else:
     print("Error opening serial port")
 
-
-write_line = "hello world!"
+# Open our unit testing file
+file = open('unit_tests.txt', 'r')
+lines = file.readlines()
 
 # Need some kind of loop to read from a .txt file
-while 1 :
+for line in lines:
 
     # Can provide the command to exit application
-    if write_line == 'exit':
-        ser.close()
-        exit()
+    if line == 'exit\n':
+
+        break
     
     # All other (functional) commands
     else:
 
         # Need to encode line to UTF-8
-        encoded = (write_line + '\r\n').encode('UTF-8')
+        encoded = (line + '\r\n').encode('UTF-8')
 
-        # Send the character to the device
+        # Send the string to the device
         ser.write(encoded)
-        print("Sent message: " + write_line)
+        print("Sent message: " + line, end='')
 
         # Let's wait 1 second before sending 
         # another command
         time.sleep(1)
+
+# Close the serial and file I/O
+ser.close()
+file.close()
+print("Closed serial connection")
