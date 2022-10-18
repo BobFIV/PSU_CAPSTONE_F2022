6eea23792ac39c62f23d616f43c1632927c65b1b
htw5022 <69948297+htw5022@users.noreply.github.com>
2022-10-18 02:47:35 -0400

    Update index.js

--- lights/index.js
+++ lights/index.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
