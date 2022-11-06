ee4c79f40e85ae59e993e941047f99fa8f7882d4
NoahSchiro <noahschiro@rocketmail.com>
2022-11-06 15:21:06 -0500

    Extended tester capabilities

--- tester/tester.py
+++ tester/tester.py
@@ -2,53 +2,85 @@ import sys
 import time
 import serial
 
-# Configure the serial connections (the parameters differs on the device you are connecting to)
-ser = serial.Serial(
-    port='/dev/ttyACM0',
-    baudrate=115200                #TODO: I believe this is correct
-)
+class Thingy:
 
-# Check if the serial port is open
-if ser.is_open:
-    print("Successfully opened serial port")
-else:
-    print("Error opening serial port")
+    def __init__(self, sleep_time=1.0):
+        self.ser = serial.Serial(
+            port='/dev/ttyACM0',
+            baudrate=1000000
+        )
 
-# How long we will sleep in between serial calls
-sleep_time = 1.0
+        # Check if the serial port is open
+        if self.ser.is_open:
+            print("Successfully opened serial port")
+        else:
+            print("Error opening serial port")
+        
+        # How long we will sleep in between serial calls
+        self.sleep_time = sleep_time 
 
-# Allows the user to define this via CLI
-if len(sys.argv) > 1:
-    sleep_time = float(sys.argv[1])
+    # Accepts a file descriptor and runs commands from that
+    def run_test_file(self, fd):
+
+        # Open our unit testing file
+        file = open(fd, 'r')
+        lines = file.readlines()
+
+        # Need loop to read from a .txt file
+        for line in lines:
+
+            # Can provide the command to exit application
+            if line == 'exit\n':
+
+                break
+            
+            # All other (functional) commands
+            else:
+
+                # Need to encode line to UTF-8
+                encoded = (line + '\r\n').encode('UTF-8')
+
+                # Send the string to the device
+                self.ser.write(encoded)
+                print("Sent message: " + line, end='')
+
+                # Let's wait 1 second before sending 
+                # another command
+                time.sleep(self.sleep_time)
+
+        file.close()
 
 
-# Open our unit testing file
-file = open('unit_tests.txt', 'r')
-lines = file.readlines()
+    # Accepts a custom command to send to the thingy
+    def run_test(self, command):
 
-# Need loop to read from a .txt file
-for line in lines:
+        encoded = (command + '\r\n').encode('UTF-8')
+        self.ser.write(encoded)
+        print("Sent message: " + command, end='\n')
 
-    # Can provide the command to exit application
-    if line == 'exit\n':
+        # Not sure if this is necessary, but give a
+        # bit of buffer room just in case
+        time.sleep(self.sleep_time)
 
-        break
-    
-    # All other (functional) commands
-    else:
+    def close(self):
 
-        # Need to encode line to UTF-8
-        encoded = (line + '\r\n').encode('UTF-8')
+        # Close the serial
+        self.ser.close()
+        print("Closed serial connection")
 
-        # Send the string to the device
-        ser.write(encoded)
-        print("Sent message: " + line, end='')
+if __name__ == "__main__":
 
-        # Let's wait 1 second before sending 
-        # another command
-        time.sleep(sleep_time)
+    tester = Thingy()
 
-# Close the serial and file I/O
-ser.close()
-file.close()
-print("Closed serial connection")
+    tester.run_test_file("unit_tests.txt")
+
+    print("\nSending custom commands")
+
+    tester.run_test("Hello;")
+    tester.run_test("World;")
+    tester.run_test("This;")
+    tester.run_test("Is;")
+    tester.run_test("A;")
+    tester.run_test("Test;")
+
+    tester.close()
